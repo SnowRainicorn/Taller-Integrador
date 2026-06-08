a5983dc883258ef7afbcd1381dd72b3e23e55f8c
gmejia16 <112442770+gmejia16@users.noreply.github.com>
2026-06-08 00:21:59 -0600

    Update main.cpp
    Corrección de etapa de medición de estado de batería y otros detalles menores. Versión final.

--- Tracker-Gr3-Guardaparques/src/main.cpp
+++ Tracker-Gr3-Guardaparques/src/main.cpp
@@ -6,7 +6,8 @@
 #include <TinyGPS++.h>
 #include <LoRa.h>
 #include <OneButton.h>
-#define XPOWERS_CHIP_AXP192
+
+#define XPOWERS_CHIP_AXP2101
 #include <XPowersLib.h>
 
 // ==========================================
@@ -14,10 +15,12 @@
 // ==========================================
 const String CALLSIGN = "TI0TEC3-7";
 const String APRS_COMMENT = "Grupo 3: G. Mejia, E. Valle.";
+const String TOCALL = "APRS"; // Identificador 
 
 // ==========================================
-// ASIGNACIÓN DE PINES HARDWARE (LILYGO T-BEAM 433MHz)
+// ASIGNACIÓN DE PINES HARDWARE (T-BEAM V1.2)
 // ==========================================
+
 #define SCK_LORA     5
 #define MISO_LORA    19
 #define MOSI_LORA    27
@@ -27,7 +30,7 @@ const String APRS_COMMENT = "Grupo 3: G. Mejia, E. Valle.";
 
 #define GPS_RX_PIN   34
 #define GPS_TX_PIN   12
-#define BUTTON_PIN   38
+#define BUTTON_PIN   38  
 
 #define OLED_SDA     21
 #define OLED_SCL     22
@@ -46,7 +49,7 @@ XPowersPMU PMU;
 // ==========================================
 // MÁQUINA DE ESTADOS Y BANDERAS FINITAS
 // ==========================================
-enum EstadosTracker { INIT, GESTION_ENERGIA, LEER_GPS, EVALUAR_EVENTO, EMPAQUETAR, TRANSMITIR, DORMIR };
+enum EstadosTracker { INIT, LEER_GPS, EVALUAR_EVENTO, EMPAQUETAR, TRANSMITIR, DORMIR };
 EstadosTracker estado_actual = INIT;
 
 enum PerfilUso { MODO_CAMINANDO, MODO_BOTE };
@@ -62,15 +65,17 @@ bool modo_ahorro_pantalla = false;
 
 uint32_t tiempo_ultima_tx = 0;
 uint32_t tiempo_pantalla_encendida = 0;
+uint32_t tiempo_ultima_lectura_pmu = 0;
 uint32_t timeout_gps_ms = 120000; 
 
 String tipo_mensaje_actual = "RUTINA";
 String ultima_rx_callsign = "Ninguno";
-int porcentaje_bateria = 100;
+int porcentaje_bateria = 0;
 
 // ==========================================
 // VECTORES GRÁFICOS (16x16 px)
 // ==========================================
+
 const unsigned char icon_walk[] PROGMEM = {
   0x01, 0x80, 0x01, 0x80, 0x01, 0x80, 0x03, 0xc0, 0x07, 0x00, 0x0e, 0x00,
   0x18, 0x00, 0x0f, 0xc0, 0x01, 0x80, 0x03, 0x00, 0x06, 0x00, 0x0c, 0x00,
@@ -86,6 +91,7 @@ const unsigned char icon_boat[] PROGMEM = {
 // ==========================================
 // RUTINAS DE CONVERSIÓN TOPOGRÁFICA APRS
 // ==========================================
+
 String paddingCeros(int numero, int ancho) {
     String res = String(numero);
     while (res.length() < ancho) res = "0" + res;
@@ -111,6 +117,7 @@ String conversionLongitudAPRS(double lon) {
 // ==========================================
 // CONTROL DE UI Y EVENTOS DE INTERRUPCIÓN
 // ==========================================
+
 void renderizarPantallaPrincipal() {
     if (modo_ahorro_pantalla) {
         display.clearDisplay();
@@ -125,14 +132,11 @@ void renderizarPantallaPrincipal() {
     display.setCursor(0, 0);
     display.print(CALLSIGN);
     
-    if (perfil_actual == MODO_CAMINANDO) {
-        display.drawBitmap(112, 0, icon_walk, 16, 16, WHITE);
-    } else {
-        display.drawBitmap(112, 0, icon_boat, 16, 16, WHITE);
-    }
+    if (perfil_actual == MODO_CAMINANDO) display.drawBitmap(112, 0, icon_walk, 16, 16, WHITE);
+    else display.drawBitmap(112, 0, icon_boat, 16, 16, WHITE);
 
     display.setTextSize(1);
-
+    
     display.setCursor(0, 18);
     if (gps.time.isValid() && gps.date.isValid()) {
         int hora_local = gps.time.hour() - 6;
@@ -144,27 +148,53 @@ void renderizarPantallaPrincipal() {
     }
 
     display.setCursor(0, 30);
-    if (gps.location.isValid()) {
-        display.print(String(gps.location.lat(), 4) + "  " + String(gps.location.lng(), 4));
-    } else {
-        display.print("No Fix");
-    }
+    if (gps.location.isValid()) display.print(String(gps.location.lat(), 4) + "  " + String(gps.location.lng(), 4));
+    else display.print("No Fix");
 
     display.setCursor(0, 42);
     display.print("A=" + paddingCeros((int)gps.altitude.meters(), 4) + "m    " + String((int)gps.speed.kmph()) + "km/h");
 
     display.setCursor(0, 54);
     display.print("Rx:" + ultima_rx_callsign);
-    display.setCursor(85, 54);
-    display.print("B:" + String(porcentaje_bateria) + "%");
+    
+    display.setCursor(68, 54);
+    if (PMU.isVbusIn()) display.print("Cargando...");
+    else display.print("B: " + String(porcentaje_bateria) + "%");
     
     display.display();
+}
+
+void actualizarEnergia() {
+    // Muestreo asíncrono para garantizar actualización en tiempo real
+    if (PMU.isBatteryConnect()) {
+        uint16_t vol_mv = PMU.getBattVoltage();
+        if (vol_mv > 0) {
+            float voltaje = vol_mv / 1000.0;
+            porcentaje_bateria = (int)(((voltaje - 3.2) / (4.2 - 3.2)) * 100.0);
+            if (porcentaje_bateria > 100) porcentaje_bateria = 100;
+            if (porcentaje_bateria < 0) porcentaje_bateria = 0;
+        } else {
+            porcentaje_bateria = 0;
+        }
+    } else {
+        porcentaje_bateria = 0;
+    }
+    
+    if (porcentaje_bateria < 15 && !PMU.isVbusIn() && PMU.isBatteryConnect()) {
+        bandera_bateria_baja = true;
+    } else {
+        bandera_bateria_baja = false;
+    }
+
+    if (!modo_ahorro_pantalla && !bandera_sos) {
+        renderizarPantallaPrincipal();
+    }
 }
 
 void clickSimpleBoton() {
     modo_ahorro_pantalla = false;
     tiempo_pantalla_encendida = millis();
-    renderizarPantallaPrincipal();
+    actualizarEnergia();
 }
 
 void dobleClickBoton() {
@@ -174,8 +204,16 @@ void dobleClickBoton() {
 
 void presionLargaBoton() {
     bandera_sos = true;
-    clickSimpleBoton();
+    modo_ahorro_pantalla = false;
+    tiempo_pantalla_encendida = millis();
     estado_actual = EVALUAR_EVENTO; 
+    
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(15, 25);
+    display.print("SOS !!!");
+    display.display();
 }
 
 // ==========================================
@@ -185,11 +223,18 @@ void setup() {
     Serial.begin(115200);
     Wire.begin(OLED_SDA, OLED_SCL);
     
-    if (PMU.begin(Wire, AXP192_SLAVE_ADDRESS, OLED_SDA, OLED_SCL)) {
-        PMU.setLDO2Voltage(3300); 
-        PMU.enableLDO2();
-        PMU.setLDO3Voltage(3300); 
-        PMU.enableLDO3();
+    if (PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, OLED_SDA, OLED_SCL)) {
+        PMU.setALDO2Voltage(3300);
+        PMU.enableALDO2();
+        PMU.setALDO3Voltage(3300);
+        PMU.enableALDO3();
+
+        PMU.enableBattDetection();
+        PMU.enableVbusVoltageMeasure();
+        PMU.enableBattVoltageMeasure();
+        
+        PMU.setChargeTargetVoltage(XPOWERS_AXP2101_CHG_VOL_4V2);
+        PMU.setChargerConstantCurr(XPOWERS_AXP2101_CHG_CUR_500MA); 
     }
 
     if (display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
@@ -199,7 +244,7 @@ void setup() {
 
     userButton.attachClick(clickSimpleBoton);
     userButton.attachDoubleClick(dobleClickBoton);
-    userButton.setPressMs(6000); 
+    userButton.setPressMs(3000); 
     userButton.attachLongPressStart(presionLargaBoton);
 
     gpsSerial.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
@@ -208,7 +253,7 @@ void setup() {
     LoRa.setPins(NSS_LORA, RST_LORA, DIO0_LORA);
     
     if (!LoRa.begin(433775000)) { 
-        while (1);
+        while (1); 
     }
     
     LoRa.setTxPower(20, PA_OUTPUT_PA_BOOST_PIN); 
@@ -217,7 +262,7 @@ void setup() {
     LoRa.setCodingRate4(5);
     LoRa.enableCrc();
     
-    estado_actual = GESTION_ENERGIA;
+    estado_actual = LEER_GPS;
 }
 
 // ==========================================
@@ -226,37 +271,39 @@ void setup() {
 void loop() {
     userButton.tick();
 
-    if (!modo_ahorro_pantalla && (millis() - tiempo_pantalla_encendida >= 20000)) { 
+    // Rutina asíncrona: Muestrea datos energéticos y refresca OLED cada 2000 ms
+    if (millis() - tiempo_ultima_lectura_pmu >= 2000) {
+        actualizarEnergia();
+        tiempo_ultima_lectura_pmu = millis();
+    }
+
+    // Temporizador de ahorro de pantalla
+    if (!modo_ahorro_pantalla && !bandera_sos && (millis() - tiempo_pantalla_encendida >= 20000)) { 
         modo_ahorro_pantalla = true;
         renderizarPantallaPrincipal();
     }
 
     switch (estado_actual) {
         case INIT:
-            estado_actual = GESTION_ENERGIA;
-            break;
-
-        case GESTION_ENERGIA: {
-            porcentaje_bateria = PMU.isBatteryConnect() ? PMU.getBatteryPercent() : 100;
-            
-            if (porcentaje_bateria < 15 && !PMU.isVbusIn()) {
-                bandera_bateria_baja = true;
-            } else {
-                bandera_bateria_baja = false;
-            }
-            renderizarPantallaPrincipal();
             estado_actual = LEER_GPS;
             break;
-        }
 
         case LEER_GPS: {
             uint32_t inicio_lectura = millis();
+            
             while (millis() - inicio_lectura < timeout_gps_ms) {
                 userButton.tick(); 
+                
+                // Refresco interno de energía durante el bloqueo del GPS
+                if (millis() - tiempo_ultima_lectura_pmu >= 2000) {
+                    actualizarEnergia();
+                    tiempo_ultima_lectura_pmu = millis();
+                }
+
                 while (gpsSerial.available() > 0) gps.encode(gpsSerial.read());
                 
                 if (gps.location.isUpdated() && gps.location.isValid()) break;
-                if (bandera_sos && gps.location.isValid()) break;
+                if (bandera_sos) break;
             }
             estado_actual = EVALUAR_EVENTO;
             break;
@@ -272,7 +319,8 @@ void loop() {
         case EMPAQUETAR: {
             char simbolo_mapa = (perfil_actual == MODO_CAMINANDO) ? SYMBOL_WALKING : SYMBOL_BOAT;
             
-            String trama_aprs_final = CALLSIGN + ">APRS,WIDE1-1:=";
+            // Corrección de la cabecera AX.25: El TOCALL y el PATH deben estar separados correctamente
+            String trama_aprs_final = CALLSIGN + ">" + TOCALL + ",WIDE1-1,qAR:=";
             
             if (gps.location.isValid()) {
                 trama_aprs_final += conversionLatitudAPRS(gps.location.lat());
@@ -299,7 +347,6 @@ void loop() {
             
             if (bandera_sos) bandera_sos = false;
             tiempo_ultima_tx = millis();
-            renderizarPantallaPrincipal();
             
             if (!bandera_bateria_baja) LoRa.receive(); 
             else LoRa.sleep();
@@ -328,7 +375,7 @@ void loop() {
             }
             
             if (millis() - tiempo_ultima_tx >= intervalo) {
-                estado_actual = GESTION_ENERGIA;
+                estado_actual = LEER_GPS;
             }
             break;
         }
